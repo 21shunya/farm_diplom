989ee32b21ce5b91fa71d5b50ba94a30d890e269
shunya <shabanchick@gmail.com>
2022-11-17 14:15:08 +0300

    fix: update arrow icons

--- src/assets/icons/ArrowLeft.tsx
+++ src/assets/icons/ArrowLeft.tsx
@@ -2,11 +2,19 @@ import React from 'react';
 
 interface IArrowLeft {
   color: string;
+  size?: number;
+  onClick?: () => void;
 }
 
-const ArrowLeft: React.FC<IArrowLeft> = ({ color }) => {
+const ArrowLeft: React.FC<IArrowLeft> = ({ color, size }) => {
   return (
-    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg
+      width={size || 24}
+      height={size || 24}
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
       <path d="M14 18L15.41 16.59L10.83 12L15.41 7.41L14 6L8 12L14 18Z" fill={color || 'white'} />
     </svg>
   );
--- src/assets/icons/ArrowRight.tsx
+++ src/assets/icons/ArrowRight.tsx
@@ -2,11 +2,19 @@ import React from 'react';
 
 interface IArrowRight {
   color: string;
+  size?: number;
+  onClick?: () => void;
 }
 
-const ArrowRight: React.FC<IArrowRight> = ({ color }) => {
+const ArrowRight: React.FC<IArrowRight> = ({ color, size }) => {
   return (
-    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg
+      width={size || 24}
+      height={size || 24}
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
       <path d="M9.41 6L8 7.41L12.58 12L8 16.59L9.41 18L15.41 12L9.41 6Z" fill={color || 'white'} />
     </svg>
   );
